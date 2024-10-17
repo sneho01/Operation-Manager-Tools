7e72c7dec6e48fd732008ba433dffe1df9b372e8
sneho01 <masudparvessneho@outlook.com>
2024-10-18 01:22:53 +0600

    Added bonus feature

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-    float x1, x2, ans;
+    float x1, x2, y1, y2, ans;
     int opt;
     printf("\t****************************************\n");
     printf("\t*\tOperations Manager Tool        *\n");
@@ -10,18 +12,35 @@ int main()
     printf("\t*\t3. Multiply                    *\n");
     printf("\t*\t4. Divide                      *\n");
     printf("\t*\t5. Modulus                     *\n");
-    printf("\t*\t6. Exit                        *\n");
+    printf("\t*\t6. Absolute Difference         *\n");
+    printf("\t*\t7. Squre Root Calculation      *\n");
+    printf("\t*\t8. Distance Calculation        *\n");
+    printf("\t*\t9. Exit                        *\n");
     printf("\t****************************************\n");
-    printf("Select an option (1-6): ");
+    printf("Select an option (1-9): ");
     scanf("%d", &opt);
 
-if(opt < 1 || opt > 6){
+if(opt < 1 || opt > 9){
     printf("Invalid selection. Please select a number between 1 and 6.");
     return 0;
-}else if (opt == 6){
+}else if (opt == 9){
     printf("Exiting the program. Goodbye!");
     return 0;
-}else{
+}else if(opt == 7){
+    printf("Enter a non negative number: ");
+    scanf("%f", &x1);
+}else if(opt == 8){
+    printf("Enter the coordinates\n");
+    printf("x1 --> ");
+    scanf("%f", &x1);
+    printf("y2 --> ");
+    scanf("%f", &y2);
+    printf("x2 --> ");
+    scanf("%f", &x2);
+    printf("y2 --> ");
+    scanf("%f", &y2);
+}
+else{
     printf("Enter the first number: ");
     scanf("%f", &x1);
     printf("Enter the second number: ");
@@ -58,6 +77,16 @@ case 5:
     }
     printf("The result of %d %% %d (from %.2lf %% %.2lf) is: %d", (int)x1, (int)x2, x1, x2, (int)ans);
     return 0;
+case 6:
+    ans = x1 - x2;
+    if(ans < 0){
+        ans = -ans;
+    }
+case 7:
+    ans = sqrt(x1);
+case 8:
+    ans = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    printf("The distance between the points is: %.2f", ans);
 default:
     break;
 }
